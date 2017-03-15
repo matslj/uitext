cec32392c33e07b1ce0aea60aa200f74de2c507e
Mats Ljungquist <none@none.se>
2017-03-15 02:04:26 +0100

    Show hide sidebar fixed

---
+++ src/main/java/se/mlj/uitext/model/user/SessionState.java
@@ -0,0 +1,31 @@
+package se.mlj.uitext.model.user;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped
+@Named
+public class SessionState implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private boolean showSidebar = true;
+
+	public boolean isShowSidebar() {
+		return showSidebar;
+	}
+
+	public void setShowSidebar(boolean showSidebar) {
+		this.showSidebar = showSidebar;
+	}
+	
+	public void toggleSidebar() {
+		showSidebar = !showSidebar;
+	}
+
+}
