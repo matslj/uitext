20f268a980dcb062bebcd63d2e0e9e066b6ee173
Mats Ljungquist <noreply@nonsense.email.se>
2017-05-30 15:55:05 +0200

    Updates

--- src/main/java/se/mlj/uitext/model/car/BasicView.java
+++ src/main/java/se/mlj/uitext/model/car/BasicView.java
@@ -1,11 +1,15 @@
 package se.mlj.uitext.model.car;
 
+import java.io.IOException;
 import java.io.Serializable;
+import static org.omnifaces.util.Faces.redirect;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+
+import org.primefaces.event.SelectEvent;
  
 @ManagedBean(name="dtBasicView")
 @ViewScoped
@@ -16,6 +20,8 @@ public class BasicView implements Serializable {
     @ManagedProperty("#{carService}")
     private CarService service;
  
+    private Car selectedCar;
+    
     @PostConstruct
     public void init() {
         cars = service.createCars(10);
@@ -28,4 +34,17 @@ public class BasicView implements Serializable {
     public void setService(CarService service) {
         this.service = service;
     }
+    
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+ 
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }
+ 
+    public void onRowSelect(SelectEvent event) throws IOException {
+        redirect("/planer/plan");
+    }
+
 }
